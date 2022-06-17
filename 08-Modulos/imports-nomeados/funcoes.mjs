75cb8607a71c93fcc87ae068821de4a983d8d12f
0isouza <igorsouza457@gmail.com>
2022-06-17 00:33:33 -0300

    Desafio aula prática 08 solucionado

---
+++ 08-Modulos/imports-nomeados/funcoes.mjs
@@ -0,0 +1,14 @@
+
+function mostraIdade(nome,idade){
+    return (`A idade de ${nome} é ${idade}`);
+}
+
+function mostraCidade(cidade,estado){
+    return (`A cidade ${cidade} fica no estado ${estado}`);
+}
+
+function mostraHobby(nome,hobby){
+    return (`O Hobby de ${nome} é ${hobby}`);
+}
+
+export {mostraIdade, mostraCidade, mostraHobby};
---
+++ 08-Modulos/imports-nomeados/main.mjs
@@ -0,0 +1,6 @@
+
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Igor', 20));
+console.log(mostraCidade('Jundiaí','São Paulo'));
+console.log(mostraHobby('Igor','tocar bateria'));
